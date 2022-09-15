1af6067ac9543f14e3edad60aeb2b11c25129008
Nikita-Minov <nikitachryackhov555@gmail.com>
2022-09-15 21:08:04 +0300

    Добавьте файлы проекта.

---
+++ Lab01/main.c
@@ -0,0 +1,19 @@
+﻿#include <locale.h>
+#include <stdio.h>
+#include "main.h"
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	// 1.1
+	puts("Моя программа");
+
+	// 1.2
+	puts("Hello world!");
+
+	// 2 
+	name();
+
+	//3
+	date();
+}
